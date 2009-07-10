3c09ea7427169d98f205b875c0901ad1422c6076
Tom <tomheefteenwebsite@hotmail.com>
2009-07-10 20:40:28 +0000

    git-svn-id: file:///home/svn/notefly/trunk@32 a263dc91-4059-394b-934f-0a419a0b4477

---
+++ skin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace SimplePlainNote
+{
+    class skin
+    {
+        private int notecolor = 0;
+
+        #region constructor
+        public skin(int numcolor)
+        {
+            this.notecolor = numcolor;
+        }
+        #endregion
+
+
+        public Color getObjColor(bool selected)
+        {
+            switch (this.notecolor)
+            {
+                case 0:
+                    if (selected) return Color.Orange;
+                    else return Color.Gold;
+                case 1:
+                    if (selected) return Color.DarkOrange;
+                    else return Color.Orange;
+                case 2:
+                    if (selected) return Color.Gray;
+                    else return Color.White;
+                case 3:
+                    if (selected) return Color.Green;
+                    else return Color.LightGreen;
+                case 4:
+                    if (selected) return Color.Blue;
+                    else return Color.CornflowerBlue;
+                case 5:
+                    if (selected) return Color.Purple;
+                    else return Color.Magenta;
+                case 6:
+                    if (selected) return Color.DarkRed;
+                    else return Color.Red;
+                default:
+                    return Color.Gold;
+            }
+        }
+
+        public Color getObjColor(bool selected, bool highlight, bool warn)
+        {
+            if (highlight)
+            {
+                return Color.LightYellow;                
+            }
+            else if (warn)
+            {
+                return Color.Red;
+            }
+            else
+            {
+                return getObjColor(selected);
+            }
+        }
+    }
+}
