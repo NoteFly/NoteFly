61baacaf44cbf7b97dbfb065f1d228ce5fdee86f
Tom <tomheefteenwebsite@hotmail.com>
2009-09-11 08:59:03 +0000

    git-svn-id: file:///home/svn/notefly/trunk@81 a263dc91-4059-394b-934f-0a419a0b4477

---
+++ make_archive.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Please make sure 7-zip is in your PATH or 
+REM put the full path to 7z cmd
+
+echo Making zip archive...
+
+set SPN_SCR=M:\Public\sourcecode\Projects_Csharp\simpleplainnote
+set VERSION="0.9.5"
+
+C:\Libs\mozilla-build\7zip\7z.exe a -tzip %SPN_SCR%\bin\Release\scr_SimplePlainNote_v%VERSION%.zip -r %SPN_SCR%\*.cs %SPN_SCR%\*.resx %SPN_SCR%\*.config %SPN_SCR%\*.sln %SPN_SCR%\*.csproj %SPN_SCR%\*.bat  %SPN_SCR%\*.png %SPN_SCR%\*.ico %SPN_SCR%\*.bat %SPN_SCR%\*.xsd %SPN_SCR%\*.manifest %SPN_SCR%\*.nsi %SPN_SCR%\*.settings %SPN_SCR%\*.zargo %SPN_SCR%\*.csproj.user %SPN_SCR%\*.txt
+
+echo done.
+
+pause
