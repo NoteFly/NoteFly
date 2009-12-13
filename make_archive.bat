07784d7eb5013c29b01a905cdafa8ccd78695052
Tom <tomheefteenwebsite@hotmail.com>
2009-12-13 01:18:57 +0000

    update archive script.
    git-svn-id: file:///home/svn/notefly/trunk@135 a263dc91-4059-394b-934f-0a419a0b4477

--- make_archive.bat
+++ make_archive.bat
@@ -1,17 +1,19 @@
 @echo off
-echo Making zip archive...
+set path7z="C:\Libs\mozilla-build\7zip\7z.exe"
+set VERSION=1.0.0
 
-set VERSION="0.9.10"
-set 7zpath="C:\Libs\mozilla-build\7zip\7z.exe"
-set %SPN_SCR%="%cd%"
+echo Making zip archive: src_NoteFly_v%VERSION%.zip
+echo 7-zip(7z.exe) should be installed in: %path7z%
 
-IF NOT EXIST C:\Libs\mozilla-build\7zip\7z.exe GOTO NO7ZDIR
+pause
+IF NOT EXIST %path7z% GOTO NO7ZDIR
 
-REM Please make sure 7-zip is in your PATH or put the full path to 7z.exe
-%7zpath% a -tzip "%SPN_SCR%\bin\Release\source archives\src_NoteDesk_v%VERSION%.zip" -r %SPN_SCR%\*.cs %SPN_SCR%\*.resx %SPN_SCR%\*.config %SPN_SCR%\*.sln %SPN_SCR%\*.csproj %SPN_SCR%\*.bat  %SPN_SCR%\*.png %SPN_SCR%\*.ico %SPN_SCR%\*.bat %SPN_SCR%\*.xsd %SPN_SCR%\*.manifest %SPN_SCR%\*.nsi %SPN_SCR%\*.settings %SPN_SCR%\*.zargo %SPN_SCR%\*.csproj.user %SPN_SCR%\*.txt
+%path7z% a -tzip "%cd%\bin\Release\source archives\src_NoteFly_v%VERSION%.zip" -r %cd%\*.cs %cd%\*.resx %cd%\*.config %cd%\*.sln %cd%\*.csproj %cd%\*.eqconfig%cd%\*.png %cd%\*.ico %cd%\*.bat %cd%\*.xsd %cd%\*.manifest %cd%\*.nsi %cd%\*.settings %cd%\*.zargo %cd%\*.uml %cd%\*.csproj.user %cd%\*.pkgxml
 
 echo done.
 pause
+exit
 
 :NO7ZDIR
-echo Cannot find 7-zip. Please check whethere 7-zip is installed in: %7zpath%
+echo Cannot find 7-zip (7z.exe). Please change the path7z variable in this script.
+pause
