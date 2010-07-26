eb21d17a3a41ff815469ff39e69ad29a4804b6b1
Tom <tomheefteenwebsite@hotmail.com>
2010-07-27 00:28:53 +0200

    add some settings

--- Settings.cs
+++ Settings.cs
@@ -25,56 +25,81 @@ namespace NoteFly
         public int NotesTransparencyLevel { get; set; }
 
         public int NotesDefaultColor { get; set; }
-        
-        public int NotesTextdirection { get; set; }
-        
+
+        public bool NotesClosebtnTooltipenabled { get; set; }
+
+        public bool NotesClosebtnHidenotepermanently { get; set; }
+
         public string NotesSavepath { get; set; }
 
-        public int TrayiconActionLeftclick { get; set; }
-       
+        public int TrayiconLeftclickaction { get; set; }
+
+        public bool TrayiconCreatenotebold { get; set; }
+
+        public bool TrayiconManagenotesbold { get; set; }
+
+        public bool TrayiconSettingsbold { get; set; }
+
+        public bool TrayiconExitbold { get; set; }
+
+        public int FontTextdirection { get; set; }
+
         public string FontTitleFamily { get; set; }
-       
+
         public int FontTitleSize { get; set; }
-       
+
+        public bool FontTitleStylebold { get; set; }
+
         public string FontContentFamily { get; set; }
 
         public Decimal FontContentSize { get; set; }
 
         public bool HighlightHTML { get; set; }
-        
+
         public bool HighlightHyperlinks { get; set; }
-        
+
         public bool HighlightPHP { get; set; }
-        
+
         public bool HighlightSQL { get; set; }
 
         public bool ConfirmLinkclick { get; set; }
-        
+
         public bool ConfirmExit { get; set; }
 
         public bool ConfirmDeletenote { get; set; }
 
-        public string Defaultemail { get; set; }
-	    
+        public bool SocialEmailEnabled { get; set; }
+
+        public string SocialEmailDefaultadres { get; set; }
+
+        public bool SocialTwitterEnabled { get; set; }
+
         public string SocialTwitterUsername { get; set; }
 
         public string SocialTwitterpassword { get; set; }
 
+        public bool SocialTwitterUseSSL { get; set; } //default on and not changed throw gui.
+
+        public bool SocialFacebookEnabled { get; set; }
+
         public bool SocialFacebookSavesession { get; set; }
 
-        public bool NetworkProxyuse { get; set; }
+        public bool SocialFacebookUseSSL { get; set; }
 
-        public string NetworkProxyaddr { get; set; }
+        public int NetworkConnectionTimeout { get; set; }
 
-        public int NetworkTimeout { get; set; }
-        
+        public bool NetworkConnectionForceipv6 { get; set; } //e.g. set ipv6.facebook.com
+
+        public bool NetworkProxyEnabled { get; set; }
+
+        public string NetworkProxyAddress { get; set; }
+
         public bool ProgramFirstrun { get; set; }
-        
+
         public bool ProgramLogError { get; set; }
-        
+
         public bool ProgramLogException { get; set; }
 
         public bool ProgramLogInfo { get; set; }
-
     }
 }
