762d714b1b4c9c4d665cf85f99a41110b2e41011
Tom <tomheefteenwebsite@hotmail.com>
2010-12-20 02:12:05 +0100

    Changed the way manage note window displays the list of notes.

--- FrmManageNotes.Designer.cs
+++ FrmManageNotes.Designer.cs
@@ -19,9 +19,6 @@ namespace NoteFly
     public partial class FrmManageNotes
     {
         private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.Label lbTextNotes;
-        private System.Windows.Forms.Label lbTextNoteOptions;
-        private System.Windows.Forms.Panel pnlNotes;
         private System.Windows.Forms.Panel pnlHead;
         private System.Windows.Forms.PictureBox pbResizeGrip;
         private System.Windows.Forms.Timer timerUpdateNotesList;
@@ -56,14 +53,22 @@ namespace NoteFly
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmManageNotes));
             this.btnClose = new System.Windows.Forms.Button();
-            this.lbTextNotes = new System.Windows.Forms.Label();
-            this.lbTextNoteOptions = new System.Windows.Forms.Label();
-            this.pnlNotes = new System.Windows.Forms.Panel();
             this.pnlHead = new System.Windows.Forms.Panel();
             this.pbResizeGrip = new System.Windows.Forms.PictureBox();
             this.timerUpdateNotesList = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colShowed = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.colColor = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.pnlHead.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbResizeGrip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnClose
@@ -72,7 +77,7 @@ namespace NoteFly
             this.btnClose.BackColor = System.Drawing.Color.DarkOrange;
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Location = new System.Drawing.Point(305, 2);
+            this.btnClose.Location = new System.Drawing.Point(415, 2);
             this.btnClose.Margin = new System.Windows.Forms.Padding(0);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(32, 23);
@@ -81,54 +86,17 @@ namespace NoteFly
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // lbTextNotes
-            // 
-            this.lbTextNotes.AutoSize = true;
-            this.lbTextNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTextNotes.Location = new System.Drawing.Point(11, 8);
-            this.lbTextNotes.Name = "lbTextNotes";
-            this.lbTextNotes.Size = new System.Drawing.Size(34, 16);
-            this.lbTextNotes.TabIndex = 1;
-            this.lbTextNotes.Text = "Title";
-            this.lbTextNotes.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlHead_MouseDown);
-            // 
-            // lbTextNoteOptions
-            // 
-            this.lbTextNoteOptions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbTextNoteOptions.AutoSize = true;
-            this.lbTextNoteOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTextNoteOptions.Location = new System.Drawing.Point(178, 8);
-            this.lbTextNoteOptions.Name = "lbTextNoteOptions";
-            this.lbTextNoteOptions.Size = new System.Drawing.Size(54, 16);
-            this.lbTextNoteOptions.TabIndex = 2;
-            this.lbTextNoteOptions.Text = "Options";
-            this.lbTextNoteOptions.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlHead_MouseDown);
-            // 
-            // pnlNotes
-            // 
-            this.pnlNotes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlNotes.AutoScroll = true;
-            this.pnlNotes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.pnlNotes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlNotes.Location = new System.Drawing.Point(0, 28);
-            this.pnlNotes.Name = "pnlNotes";
-            this.pnlNotes.Size = new System.Drawing.Size(340, 233);
-            this.pnlNotes.TabIndex = 7;
-            // 
             // pnlHead
             // 
             this.pnlHead.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlHead.BackColor = System.Drawing.Color.Orange;
             this.pnlHead.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlHead.Controls.Add(this.lbTextNoteOptions);
-            this.pnlHead.Controls.Add(this.lbTextNotes);
+            this.pnlHead.Controls.Add(this.label1);
             this.pnlHead.Controls.Add(this.btnClose);
             this.pnlHead.Location = new System.Drawing.Point(0, 0);
             this.pnlHead.Name = "pnlHead";
-            this.pnlHead.Size = new System.Drawing.Size(340, 28);
+            this.pnlHead.Size = new System.Drawing.Size(451, 30);
             this.pnlHead.TabIndex = 8;
             this.pnlHead.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlHead_MouseMove);
             this.pnlHead.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlHead_MouseDown);
@@ -140,7 +108,7 @@ namespace NoteFly
             this.pbResizeGrip.BackColor = System.Drawing.Color.Transparent;
             this.pbResizeGrip.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pbResizeGrip.Image = global::NoteFly.Properties.Resources.hoekje;
-            this.pbResizeGrip.Location = new System.Drawing.Point(324, 264);
+            this.pbResizeGrip.Location = new System.Drawing.Point(435, 306);
             this.pbResizeGrip.Margin = new System.Windows.Forms.Padding(0);
             this.pbResizeGrip.Name = "pbResizeGrip";
             this.pbResizeGrip.Size = new System.Drawing.Size(16, 16);
@@ -156,16 +124,139 @@ namespace NoteFly
             this.timerUpdateNotesList.Interval = 1000;
             this.timerUpdateNotesList.Tick += new System.EventHandler(this.timerUpdateNotesList_Tick);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Wheat;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId,
+            this.colTitle,
+            this.colShowed,
+            this.colColor});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(427, 245);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Wheat;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(222, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "restore all from file";
+            this.button4.UseCompatibleTextRendering = true;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Wheat;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "show selected";
+            this.button1.UseCompatibleTextRendering = true;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Wheat;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(123, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "delete selected";
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Wheat;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(334, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "backup all to file";
+            this.button3.UseCompatibleTextRendering = true;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // colId
+            // 
+            this.colId.HeaderText = "ID";
+            this.colId.Name = "colId";
+            this.colId.Width = 30;
+            // 
+            // colTitle
+            // 
+            this.colTitle.HeaderText = "Title";
+            this.colTitle.Name = "colTitle";
+            this.colTitle.Width = 200;
+            // 
+            // colShowed
+            // 
+            this.colShowed.HeaderText = "Showed";
+            this.colShowed.MinimumWidth = 30;
+            this.colShowed.Name = "colShowed";
+            this.colShowed.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colShowed.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.colShowed.Width = 50;
+            // 
+            // colColor
+            // 
+            this.colColor.HeaderText = "Color";
+            this.colColor.Items.AddRange(new object[] {
+            "yellow",
+            "orange",
+            "green",
+            "blue",
+            "purple",
+            "white"});
+            this.colColor.Name = "colColor";
+            this.colColor.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colColor.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
             // FrmManageNotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Orange;
             this.CausesValidation = false;
-            this.ClientSize = new System.Drawing.Size(340, 280);
+            this.ClientSize = new System.Drawing.Size(451, 322);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pbResizeGrip);
             this.Controls.Add(this.pnlHead);
-            this.Controls.Add(this.pnlNotes);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -181,11 +272,23 @@ namespace NoteFly
             this.pnlHead.ResumeLayout(false);
             this.pnlHead.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbResizeGrip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTitle;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colShowed;
+        private System.Windows.Forms.DataGridViewComboBoxColumn colColor;
     }
 }
