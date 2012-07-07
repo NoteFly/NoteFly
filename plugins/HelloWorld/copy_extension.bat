9b9aa83465f2bfa9b13b23e700bcfc4cd671fdca
Tom <tomheefteenwebsite@hotmail.com>
2012-07-07 16:27:35 +0200

    remove unused copy scripts, project file post-build does copying of plugin to plugin folder after being build.

--- plugins/HelloWorld/copy_extension.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Notice: make sure helloworld.dll is compiled as "Any cpu".
-
-copy %cd%\bin\Release\helloworld.dll %cd%\..\..\bin\Release\plugins\helloworld.dll
-
-pause
--- plugins/SendToOpera/copy_extension.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Notice: make sure SendToOpera.dll is compiled as "Any cpu".
-
-copy %cd%\bin\Release\SendToOpera.dll %cd%\..\..\bin\Release\plugins\SendToOpera.dll
-
-pause
