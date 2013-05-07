59cd1fe5b35933f52f9c753f6fa079849ec1650f
Tom <releases@notefly.org>
2013-05-07 14:29:22 +0200

    update translation strings.
    Signed-off-by: Tom <releases@notefly.org>

--- build_translationfile.bat
+++ build_translationfile.bat
@@ -18,7 +18,7 @@ REM  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 REM "FrmAbout.cs" "FrmDownloader.cs" "FrmException.cs" "FrmManageNotes.cs" "FrmNewNote.cs" "FrmNote.cs" "FrmPlugins.cs" "FrmSettings.cs" "PluginGrid.cs" "SearchTextBox.cs" "Notes.cs" "PluginsManager.cs" "Program.cs" "TrayIcon.cs" "XmlUtil.cs"
 echo Extracting translations of sourcecode.
 
-xgettext --from-code=UTF-8 *.cs -k --keyword=T --language=C# -o .\bin\Strings.pot --copyright-holder="NoteFly"
+xgettext --from-code=UTF-8 *.cs -k --add-location --keyword=T --language=C# -o .\bin\Strings.pot --copyright-holder="NoteFly"
 
 echo finished
 echo.
